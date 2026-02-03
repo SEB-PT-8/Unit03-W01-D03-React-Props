9708f7100b8c75e61cd46497549767c01b65ee3a
omarakamal <omaraakamal@gmail.com>
2026-02-03 18:45:59 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Greeting from './components/Greeting'
 function App() {
 
   return (
     <>
-      
+      <h1>App Page</h1>
+      <Greeting studentName='Ebrahim' course='SEB'/>
+      <Greeting studentName='Ahmed' course='SEB'/>
     </>
   )
 }
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,16 @@
+function Greeting(props){
+    // props is ALWAYS an object
+    console.log(props.studentName)
+
+    // destructuring props
+    // const studentName = props.studentName
+    // const course = props.course
+    const {studentName, course} = props
+    return(
+        <div>
+            <h1>Hello Good Morning {studentName}, I am currently learning {course}</h1>
+        </div>
+    )
+}
+
+export default Greeting
